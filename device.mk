6fa19bad00f7574f94d95c98e878a231d1c3e6c9
Demon Singur <demonsingur@gmail.com>
2018-07-14 14:37:51 +0000

    whyred: do not build useless package
    It is compiled as dependency of ipacm

--- device.mk
+++ device.mk
@@ -401,8 +401,7 @@ PRODUCT_PACKAGES += \
     libavservices_minijail.vendor \
     libjson \
     libprotobuf-cpp-full \
-    librmnetctl \
-    libxml2
+    librmnetctl
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
