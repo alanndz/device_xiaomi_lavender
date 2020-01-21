f2d3a745bac234c5afe85eed79f6145819569181
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2020-01-21 14:37:22 +0530

    lavender: Add powerhint.json for sdm660
    Change-Id: I3591038a29326b15ce00de0051208219d9f3830d
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@gmail.com>

--- device.mk
+++ device.mk
@@ -60,6 +60,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libcamera_sdm660_shim
 
+# Power Hint
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
