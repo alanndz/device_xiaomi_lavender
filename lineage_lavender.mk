e8a2e4fb3a380b23fb1e102104002b4c4ccc3f1d
alanndz <alanndz@qmail.id>
2021-01-11 22:17:37 +0000

    lavender: update redfin fp
    Signed-off-by: alanndz <alanndz@qmail.id>

--- lineage_lavender.mk
+++ lineage_lavender.mk
@@ -37,6 +37,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 TARGET_VENDOR_PRODUCT_NAME := lavender
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="coral-user 11 RP1A.201105.002 6869500 release-keys"
+    PRIVATE_BUILD_DESC="lavender-user 10 QKQ1.190910.002 V11.0.1.0.QFGMIXM release-keys"
 
-BUILD_FINGERPRINT := "google/coral/coral:11/RP1A.201105.002/6869500:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys"
